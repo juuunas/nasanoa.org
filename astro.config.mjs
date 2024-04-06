906dad4dee83b41c38a1758fb428ae54299bf32b
juuunas <qwerty@kalli.st>
2024-04-06 20:17:37 +0300

    Switch to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
-import { defineConfig, passthroughImageService } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
-  image: {
-    service: passthroughImageService(),
-  },
+  output: "static",
   integrations: [tailwind()],
 });
