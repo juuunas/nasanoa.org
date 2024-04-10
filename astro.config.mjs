009eb1e782bf0823d3013aa2ca2b20ea6e4a1c1f
juuunas <qwerty@kalli.st>
2024-04-10 23:38:32 +0300

    Add site value to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nasanoa.org",
   output: "static",
   integrations: [tailwind(), preact()],
 });
