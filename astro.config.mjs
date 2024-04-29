0d6a40b71798205369d5cf6dabc3877148f93287
juuunas <qwerty@kalli.st>
2024-04-29 18:53:53 +0300

    Add MDX to do outbound links in markdown files correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nasanoa.org",
   output: "static",
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), mdx()]
 });
--- src/content/pages/links.md
+++
@@ -1,5 +0,0 @@
-# Links
-
-- [SoundCloud](https://soundcloud.com/nasanoa "SoundCloud") for songs.
-- [Bandcamp](https://nasanoa.bandcamp.com/ "Bandcamp") for entire releases.
-- [Last.fm](https://www.last.fm/user/jjthtrthrrge4te "Last.fm") for what I listen to.
