3bdb1372fe3da2b7d476e1a4f2c50caf8b4c849a
juuunas <qwerty@kalli.st>
2024-05-06 01:33:57 +0300

    Fix crash if localStorage isn't available in Last.fm component

--- src/components/LastfmNowPlaying.tsx
+++ src/components/LastfmNowPlaying.tsx
@@ -31,15 +31,46 @@ const initState: StateTypes = {
   cached: 0,
 };
 
+function localStorageAvailable() {
+  let storage;
+  try {
+    storage = window.localStorage;
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
 export default function LastfmNowPlaying({ username }: { username: string }) {
   const [data, setData] = useState<StateTypes>(initState);
 
   useEffect(() => {
     const fetchLastFm = () => {
-      const cached: StateTypes = JSON.parse(localStorage.getItem("np") || "{}");
-      if (cached?.artist && new Date().getTime() - cached.cached < 30000) {
-        setData(cached);
-        return;
+      if (localStorageAvailable()) {
+        const cached: StateTypes = JSON.parse(
+          localStorage.getItem("np") || "{}"
+        );
+        if (cached?.artist && new Date().getTime() - cached.cached < 30000) {
+          setData(cached);
+          return;
+        }
       }
 
       fetch(
@@ -56,10 +87,11 @@ export default function LastfmNowPlaying({ username }: { username: string }) {
           };
           if (data["@attr"]?.nowplaying) {
             setData(state);
-            localStorage.setItem("np", JSON.stringify(state));
+            if (localStorageAvailable())
+              localStorage.setItem("np", JSON.stringify(state));
           } else {
             setData(initState);
-            localStorage.removeItem("np");
+            if (localStorageAvailable()) localStorage.removeItem("np");
           }
         });
     };
